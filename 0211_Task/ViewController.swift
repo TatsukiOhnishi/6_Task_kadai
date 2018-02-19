f3ef276dec6409ce37ceac18dce5df0fbc30e8a9
大西樹 <tatsuki0129@taiseijunoMacBook.local>
2018-02-19 15:38:15 +0900

    第6章タスク管理アプリ

--- 0211_Task/ViewController.swift
+++ 0211_Task/ViewController.swift
@@ -2,24 +2,205 @@
 //  ViewController.swift
 //  0211_Task
 //
+//  Created by 大西樹 on 2018/02/18.
+//  Copyright © 2018年 tatsuki.oonishi. All rights reserved.
+//
+
+//
+//  ViewController.swift
+//  0211_Task
+//
 //  Created by 大西樹 on 2018/02/12.
 //  Copyright © 2018年 tatsuki.oonishi. All rights reserved.
 //
 
 import UIKit
+import RealmSwift   // ←追加
+import UserNotifications    // 追加
+//import BTNavigationDropdownMenu
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var RefreshButtun: UIBarButtonItem!
+    let list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    // Realmインスタンスを取得する
+    let realm = try! Realm()  // ←追加
+    
+    // DB内のタスクが格納されるリスト。
+    // 日付近い順\順でソート：降順
+    // 以降内容をアップデートするとリスト内は自動的に更新される。
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: false)  // ←追加
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: UITableViewDataSourceプロトコルのメソッド
+    // データの数（＝セルの数）を返すメソッド
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskArray.count  // ←追加する
+        //return 0
+    }
+    
+    // 各セルの内容を返すメソッド
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 再利用可能な cell を得る
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        // Cellに値を設定する.
+        let task = taskArray[indexPath.row]
+        cell.textLabel?.text = task.title
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        let dateString:String = formatter.string(from: task.date)
+        cell.detailTextLabel?.text = dateString
+        
+        return cell
+    }
+    
+    // MARK: UITableViewDelegateプロトコルのメソッド
+    // 各セルを選択した時に実行されるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "cellSegue",sender: nil)
+    }
+    
+    // セルが削除が可能なことを伝えるメソッド
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath)-> UITableViewCellEditingStyle {
+        return .delete
+    }
+    
+    // Delete ボタンが押された時に呼ばれるメソッド
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            // 削除されたタスクを取得する
+            let task = self.taskArray[indexPath.row]
+            
+            // ローカル通知をキャンセルする
+            let center = UNUserNotificationCenter.current()
+            center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
+            // データベースから削除する  // ←以降追加する
+            try! realm.write {
+                self.realm.delete(self.taskArray[indexPath.row])
+                tableView.deleteRows(at: [indexPath], with: .fade)
+                
+                
+            }
+            // 未通知のローカル通知一覧をログ出力
+            center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+                for request in requests {
+                    print("/---------------")
+                    print(request)
+                    print("---------------/")
+                }
+                
+            }
+        }
+    }
+    
+    
+    // segue で画面遷移するに呼ばれる
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let inputViewController:InputViewController = segue.destination as! InputViewController
+        
+        if segue.identifier == "cellSegue" {
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        } else {
+            let task = Task()
+            task.date = Date()
+            
+            let taskArray = realm.objects(Task.self)
+            if taskArray.count != 0 {
+                task.id = taskArray.max(ofProperty: "id")! + 1
+            }
+            
+            inputViewController.task = task
+        }
+    }
+    
+    // 入力画面から戻ってきた時に TableView を更新させる
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return list.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return list[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.textField.text = list[row]
+    }
+    
+    @objc func cancel() {
+        self.textField.text = ""
+        self.textField.endEditing(true)
+    }
+    
+    @objc func done() {
+        self.textField.endEditing(true)
+    }
+    
+    func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
+        return CGRect(x: x, y: y, width: width, height: height)
+    }
+    
+    
+    
+    @IBAction func searchButtun(_ sender: Any) {
+        //検索ボックスに文字列が入力される時に呼び出されるメソッド
+        let text = textField.text
+        // 名前が『f』ではじまるオブジェクト（大文字小文字の違いは無視）だけを取得
+        let result = realm.objects(Task.self).filter("category == %@", text)
+        print(result)
+        taskArray = result
+        tableView.reloadData()
+        
+    }
+    
+    
+    @IBAction func RefreshAction(_ sender: Any) {
+        taskArray = realm.objects(Task.self)//Task自身を呼び出す
+        tableView.reloadData() //再表示
+        
+    }
+    
+    
+    
+    
 }
+
+
 
